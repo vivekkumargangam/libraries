3d2f9c4978157e58cc9b2fcced295a5123ec2a42
vivekkumargangam <103211308+vivekkumargangam@users.noreply.github.com>
2022-10-27 14:38:47 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -4,24 +4,17 @@ def newgit(branch,repo)
 } 
 def imagebuilding(imagename,path)
 {
-  echo "${imagename}"
-  echo "${path}"
   sh 'sudo -S docker build -t ${imagename} -f Dockerfile ${path}'
 } 
 def runcontainer(containername,imagename)
 {
-  echo "${containername}"
-  echo "${imagename}" 
   sh 'sudo -S docker run --name ${containername} -itd ${imagename}'
 } 
 def deletecontainer(containername)
 {
-  echo "${containername}"
   sh 'sudo -S docker rm -f ${containername}'
 }  
 def copy(sourcepath,destinationpath)
 {
-  echo "${sourcepath}"
-  echo "${destinationpath}" 
   sh 'sudo -S cp -r ${sourcepath} ${destinationpath}'
 }
